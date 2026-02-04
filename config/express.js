991d1c1bdd9c575dd6b5a51fdb5560a6c1eac684
abbas2123 <189768001+abbas2123@users.noreply.github.com.>
2026-02-05 01:50:41 +0530

    limit updated

--- config/express.js
+++ config/express.js
@@ -19,8 +19,8 @@ module.exports = (app) => {
 
   app.use(helmet({ contentSecurityPolicy: false }));
 
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+  app.use(express.json({ limit: '50mb' }));
   app.use(cookieParser());
 
   app.use(

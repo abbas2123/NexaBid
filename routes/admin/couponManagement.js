8f6e60fe9831437307e342d7ac6446a81a97a578
abbas2123 <189768001+abbas2123@users.noreply.github.com.>
2026-02-05 00:12:49 +0530

    gitignorefixed

--- routes/admin/couponManagement.js
+++ routes/admin/couponManagement.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const authAdmin = require('../../middlewares/adminAuth');
-const adminControler = require('../../controllers/admin/couponManagement');
+const adminControler = require('../../controllers/admin/couponmanagement');
 router.get('/', authAdmin.adminProtect, adminControler.couponManagementPage);
 router.post('/create', authAdmin.adminProtect, adminControler.createCoupon);
 router.patch('/toggle/:id', authAdmin.adminProtect, adminControler.toggleCouponStatus);
